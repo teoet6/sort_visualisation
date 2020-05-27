25130ac6b8a1d4bfd7d906411cccf1b19ba78230
teoet6 <teoet@abv.bg>
2020-05-27 14:39:41 +0300

    SASHO YOU NEED TO FIX TABS YOURSELF

--- sorts/shell.js
+++ sorts/shell.js
@@ -3,47 +3,47 @@
 let shell_stop = false;
 let shell_pauses = 1;
 async function shell_sort_util(target_array, target_canvas, delay, palette) {
-    var increment = Math.floor(target_array.length / 2);
-    while (increment > 0) {
-        for (i = increment; i < target_array.length; i++) {
-            var j = i;
-            var temp = target_array[i].value;
-    
-            while (j >= increment && target_array[j-increment].value > temp) {
-                target_array[j].value = target_array[j-increment].value;
+	var increment = Math.floor(target_array.length / 2);
+	while (increment > 0) {
+		for (i = increment; i < target_array.length; i++) {
+			var j = i;
+			var temp = target_array[i].value;
+	
+			while (j >= increment && target_array[j-increment].value > temp) {
+				target_array[j].value = target_array[j-increment].value;
 				j = j - increment;
 				
-                drawArray(target_canvas, target_array, palette);
-                do{
+				drawArray(target_canvas, target_array, palette);
+				do{
 					await sleep(delay);
 					if(shell_stop) return shell_stop=false;
 				}while(shell_pauses > 0) 
-            }
-    
-            target_array[j].value = temp;
-        }
-    
-        if (increment == 2) {
-            increment = 1;
-        } else {
-            increment = parseInt(increment*5 / 11);
-        }
+			}
+	
+			target_array[j].value = temp;
+		}
+	
+		if (increment == 2) {
+			increment = 1;
+		} else {
+			increment = parseInt(increment*5 / 11);
+		}
 	}
 	drawArray(target_canvas, target_array, palette);
-                do{
-					await sleep(delay);
-					if(shell_stop) return shell_stop=false;
-				}while(shell_pauses > 0) 
-    while(!shell_stop)await sleep(delay);
+	do{
+		await sleep(delay);
+		if(shell_stop) return shell_stop=false;
+	}while(shell_pauses > 0) 
+	while(!shell_stop)await sleep(delay);
 	return shell_stop=false;
 }
 function shell_sort(target_canvas, n, delay, palette){
-    let target_array=[];
+	let target_array=[];
 	for(let i = 0 ; i < n ; i++){
 		target_array[i] = new Pilon(i+1);
 	}
 	target_array.sort(() => Math.random() - 0.5);
-    shell_sort_util(target_array, target_canvas, delay, palette);
+	shell_sort_util(target_array, target_canvas, delay, palette);
 }
 
 async function shell_reset(target_canvas, n, delay, palette){
