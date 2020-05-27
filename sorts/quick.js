f61345a121a4f38a69a0ae1aa09512fbe6c6eb6a
AlexOgn <61375132+AlexOgn@users.noreply.github.com>
2020-05-27 21:41:59 +0300

    Add files via upload

--- sorts/quick.js
+++ sorts/quick.js
@@ -4,17 +4,15 @@ let quick_stop = false;
 let quick_pauses = 1;
 var quick_target_array=[];
 function partition(left, right) {
-	var pivot   = quick_target_array[Math.floor((right + left) / 2)], //middle element
-		i	   = left, //left pointer
-		j	   = right; //right pointer
-	while (i < j) {
+	var pivot   = quick_target_array[Math.floor((right + left) / 2)], i= left,j= right;
+	while (i <= j) {
 		while (quick_target_array[i].value < pivot.value) {
 			i++;
 		}
 		while (quick_target_array[j].value > pivot.value) {
 			j--;
 		}
-		if (i < j) {
+		if (i <= j) {
 			let buff = clone(quick_target_array[i]);
 			quick_target_array[i] = clone(quick_target_array[j]);
 			quick_target_array[j] = buff;
@@ -27,45 +25,47 @@ function partition(left, right) {
 
 async function quick_sort_util(left, right, target_canvas, delay, palette) {
 	var index;
-	index = partition(left, right);
-	if (left < index - 1){
-		if(index>0){
-			quick_target_array[index-1].id=0
+	if ((right-left+1) > 1){
+		index = partition(left, right);
+		if (left < index - 1){
+			if(index>0){
+				quick_target_array[index-1].id=0
+			}
+			quick_target_array[index].id=1
+			
+			if(left>0){
+				quick_target_array[left-1].id=0
+			}
+			/////
+			quick_target_array[left].id=1
+			drawArray(target_canvas, quick_target_array, palette);
+			do{
+				await sleep(delay);
+				if(quick_stop) return quick_stop=false;
+			}while(quick_pauses > 0)
+			await quick_sort_util(left, index - 1, target_canvas, delay, palette);
+			////
 		}
-		quick_target_array[index].id=1
-		
-		if(left>0){
-			quick_target_array[left-1].id=0
+		if (index < right){
+			if(index>0){
+				quick_target_array[index-1].id=0
+			}
+			quick_target_array[index].id=1
+			
+			if(right>0){
+				quick_target_array[right-1].id=0
+			}
+			////
+			quick_target_array[right].id=1;
+			drawArray(target_canvas, quick_target_array, palette);
+			do{
+				await sleep(delay);
+				if(quick_stop) return quick_stop=false;
+			}while(quick_pauses > 0)
+			await quick_sort_util(index, right, target_canvas, delay, palette);
+			////
+			
 		}
-		/////
-		quick_target_array[left].id=1
-		drawArray(target_canvas, quick_target_array, palette);
-		do{
-			await sleep(delay);
-			if(quick_stop) return quick_stop=false;
-		}while(quick_pauses > 0)
-		await quick_sort_util(left, index - 1, target_canvas, delay, palette);
-		////
-	}
-	if (index < right){
-		if(index>0){
-			quick_target_array[index-1].id=0
-		}
-		quick_target_array[index].id=1
-		
-		if(right>0){
-			quick_target_array[right-1].id=0
-		}
-		////
-		quick_target_array[right].id=1;
-		drawArray(target_canvas, quick_target_array, palette);
-		do{
-			await sleep(delay);
-			if(quick_stop) return quick_stop=false;
-		}while(quick_pauses > 0)
-		await quick_sort_util(index, right, target_canvas, delay, palette);
-		////
-		
 	}
 }
 async function quick_sort(target_canvas, n, delay, palette){
